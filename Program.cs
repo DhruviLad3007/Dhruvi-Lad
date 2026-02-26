f24975af44c78ad800700ca7997b77d4585e46df
DhruviLad3007 <laddhruvi446@gmail.com>
2026-02-26 11:30:14 +0530

    Assessment:4 Environment-Based Configuration and Services (ASP .net Core)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using EnvConfigApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Task 3: Dependency Injection
+builder.Services.AddScoped<IMessageService, MessageService>();
+
+var app = builder.Build();
+
+// Configure HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+app.Run();
